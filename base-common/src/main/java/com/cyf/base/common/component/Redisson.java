a210ed218b1dd6a6bd001a07de4274e8ddd6e272
chenyf <yufeng629@163.com>
2017-06-07 11:37:58 +0800

    完善redisson的注释说明

--- base-common/src/main/java/com/cyf/base/common/component/Redisson.java
+++ base-common/src/main/java/com/cyf/base/common/component/Redisson.java
@@ -8,12 +8,19 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * @Title：操作redis的类
- * @Description：RedissonClient是redis的java客户端，能够提供多种分布式对象，特别适合在分布式环境下使用
+ * @Description：RedissonClient是redis的java客户端，跟redis连接只是其最基本的功能，并且还提供自动重连的机制，除此之外，
+ *      它还提供多种分布式对象，特别适合在分布式环境下使用，RSet、RList、RMap、RLock等等对象的使用方法，完全跟java原生的
+ *      Set、List、Lock的使用方法一样，因为Redisson提供的对象都是有重写父类中的方法的，都是能够支持分布式操作的，而对于分布式锁
+ *      {@link org.redisson.RedissonLock}，如果客户端连接断开，则会自动释放锁
+ *
  * @Author： chenyf
  * @Version： V1.0
  * @Date： 2017/5/11 16:46
  */
 public class Redisson {
+    /**
+     * 实际上实例化的是 {@link org.redisson.Redisson} 这个类，这个类是 {@link org.redisson.api.RedissonClient} 的实现类
+     */
     private RedissonClient redissonClient;
 
     //构造方法私有化
@@ -28,6 +35,7 @@ public class Redisson {
         }else{//其他情况都只启用单点模式
             config.useSingleServer().setPassword(password).setAddress(urlArr[0]);
         }
+        //create方法实际返回的是 org.redisson.Redisson 这个类对象，这个类是 org.redisson.api.RedissonClient 的实现类
         instance.redissonClient = org.redisson.Redisson.create(config);
         return instance;
     }
@@ -63,7 +71,7 @@ public class Redisson {
     /**
      * 获取通用对象桶
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonBucket}
      */
     public <T> RBucket<T> getRBucket(String objectName){
         RBucket<T> bucket = redissonClient.getBucket(objectName);
@@ -73,7 +81,7 @@ public class Redisson {
     /**
      * 获取Map对象
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonMap}
      */
     public <K,V> RMap<K, V> getRMap(String objectName){
         RMap<K, V> map = redissonClient.getMap(objectName);
@@ -83,7 +91,7 @@ public class Redisson {
     /**
      * 获取有序集合
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonSortedSet}
      */
     public <V> RSortedSet<V> getRSortedSet(String objectName){
         RSortedSet<V> sortedSet = redissonClient.getSortedSet(objectName);
@@ -93,7 +101,7 @@ public class Redisson {
     /**
      * 获取集合
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonSet}
      */
     public <V> RSet<V> getRSet(String objectName){
         RSet<V> rSet = redissonClient.getSet(objectName);
@@ -103,7 +111,7 @@ public class Redisson {
     /**
      * 获取列表
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonList}
      */
     public <V> RList<V> getRList(String objectName){
         RList<V> rList = redissonClient.getList(objectName);
@@ -113,7 +121,7 @@ public class Redisson {
     /**
      * 获取队列
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonQueue}
      */
     public <V> RQueue<V> getRQueue(String objectName){
         RQueue<V> rQueue = redissonClient.getQueue(objectName);
@@ -123,7 +131,7 @@ public class Redisson {
     /**
      * 获取双端队列
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonDeque}
      */
     public <V> RDeque<V> getRDeque(String objectName){
         RDeque<V> rDeque = redissonClient.getDeque(objectName);
@@ -132,7 +140,7 @@ public class Redisson {
 
     /**
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonBlockingQueue}
      */
     public <V> RBlockingQueue<V> getRBlockingQueue(String objectName){
         RBlockingQueue rb = redissonClient.getBlockingQueue(objectName);
@@ -142,7 +150,7 @@ public class Redisson {
     /**
      * 获取锁
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonLock}
      */
     public RLock getRLock(String objectName){
         RLock rLock = redissonClient.getLock(objectName);
@@ -152,7 +160,7 @@ public class Redisson {
     /**
      * 获取信号量
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonSemaphore}
      */
     public RSemaphore RSemaphore(String objectName){
         RSemaphore semaphore = redissonClient.getSemaphore(objectName);
@@ -162,7 +170,7 @@ public class Redisson {
     /**
      * 获取原子数
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonAtomicLong}
      */
     public RAtomicLong getRAtomicLong(String objectName){
         RAtomicLong rAtomicLong = redissonClient.getAtomicLong(objectName);
@@ -172,7 +180,7 @@ public class Redisson {
     /**
      * 获取记数锁
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonCountDownLatch}
      */
     public RCountDownLatch getRCountDownLatch(String objectName){
         RCountDownLatch rCountDownLatch = redissonClient.getCountDownLatch(objectName);
@@ -182,7 +190,7 @@ public class Redisson {
     /**
      * 获取消息的Topic
      * @param objectName
-     * @return
+     * @return {@link org.redisson.RedissonTopic}
      */
     public <M> RTopic<M> getRTopic(String objectName){
         RTopic<M> rTopic = redissonClient.getTopic(objectName);
